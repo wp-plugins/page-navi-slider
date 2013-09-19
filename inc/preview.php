b1792254067289edcc10175dd09a5fd065a260ba
Iznogood1 <Iznogood1@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-19 08:30:30 +0000

    Preview before saving Code optimization
    git-svn-id: https://plugins.svn.wordpress.org/page-navi-slider/tags/1.1@775159 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/preview.php
@@ -0,0 +1,19 @@
+<?php	if (($this->_options['wpns_preview_pages'] == 1) and ($this->_options['wpns_display_if_one_page'] !='1')) : ?>
+
+	<i><?php _e('Nothing to display as there is only one page','page-navi-slider');?><br>
+	<?php _e('You can change the &ldquo;Display even if there is only one page&rdquo; option in general settings','page-navi-slider');?></i>
+	
+<?php	else: ?>
+	<div style="border : 1px dotted #C4C4C4; background-color : <?php echo $this->_options['wpns_preview_background_color']; ?>"><i> <?php _e('Your page','page-navi-slider'); ?></i>
+		<?php
+			$settings = get_option('wpns_settings');
+			for ($i = 0; $i < $settings['wpns_preview_pages']; $i++){
+				$pagination[$i]='<a class="page-numbers" href="#">'.($i+1).'</a>';
+			}
+			wpns_frontend($settings['wpns_preview_current'],$settings['wpns_preview_pages'],$pagination,$settings,true);
+		?>
+		<i><?php _e('rest of your page...','page-navi-slider'); ?></i>
+	</div>
+
+<?php endif; ?>
+
